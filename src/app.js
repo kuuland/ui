8f01a36799b99cb74d595aac96f18b7237520e8d
yinfxs <yinfxs@gmail.com>
2019-07-17 17:44:06 +0800

    feat: 添加messageHandlers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
-import { L } from 'kuu-tools'
+import { L, config } from 'kuu-tools'
 import { message } from 'antd'
 import 'fano-antd/lib/index.less'
 
 message.config({ maxCount: 1 })
+config({
+  messageHandlers: {
+    error: msg => message.error(msg)
+  }
+})
 window.L = L
 
 export const dva = {
