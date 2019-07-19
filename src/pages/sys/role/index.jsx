89a92283eff52408b385686cb1f02d42fdf9d7a6
yinfxs <yinfxs@gmail.com>
2019-07-19 18:17:40 +0800

    feat: 优化角色表单页面

--- src/pages/sys/role/index.jsx
+++ src/pages/sys/role/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Icon } from 'antd'
-import { FanoTable } from 'fano-antd'
+import { Icon, Input, Select, Button } from 'antd'
+import { FanoTable, FanoTreeSelect } from 'fano-antd'
 import { parseIcon } from 'kuu-tools'
+import _ from 'lodash'
 import moment from 'moment'
 import styles from './index.less'
 
@@ -34,53 +35,169 @@ class Role extends React.Component {
         {
           name: 'Name',
           type: 'input',
-          label: '角色名称'
+          label: '角色名称',
+          props: {
+            fieldOptions: {
+              rules: [
+                {
+                  required: true,
+                  message: window.L('请输入角色名称')
+                }
+              ]
+            }
+          }
         },
         {
           name: 'Code',
           type: 'input',
-          label: '角色编码'
+          label: '角色编码',
+          props: {
+            fieldOptions: {
+              rules: [
+                {
+                  required: true,
+                  message: window.L('请输入角色编码')
+                }
+              ]
+            }
+          }
         },
         {
-          name: 'OperationPrivileges2',
+          name: 'ViewOperationPrivileges',
           type: 'treeselect',
           label: '操作权限',
           props: {
             url: '/api/menu?range=ALL&sort=Sort',
             titleKey: 'Name',
-            valueKey: 'ID',
+            valueKey: 'Code',
             arrayToTree: true,
             multiple: true,
             treeCheckable: true,
-            titleRender: (title, item) => <span><Icon {...parseIcon(item.Icon)} /> {title}</span>
+            titleRender: (title, item) => <span><Icon {...parseIcon(item.Icon)} /> {title}</span>,
+            fieldOptions: {
+              rules: [
+                {
+                  required: true,
+                  message: window.L('请设置操作权限')
+                }
+              ]
+            }
+          }
+        },
+        {
+          name: 'DataPrivileges',
+          type: 'render',
+          label: '数据权限',
+          props: {
+            render: props => {
+              return (
+                <div>
+                  {(props.value || []).map((item, index) => {
+                    return (
+                      <div
+                        className={styles.dpItem}
+                        key={item.key || item.ID}
+                      >
+                        <Input.Group
+                          compact
+                        >
+                          <FanoTreeSelect
+                            config={{
+                              props: {
+                                style: { width: 120 },
+                                url: '/api/org?range=ALL&sort=Sort',
+                                titleKey: 'Name',
+                                valueKey: 'ID',
+                                arrayToTree: true,
+                                placeholder: window.L('请选择组织')
+                              }
+                            }}
+                            value={item.TargetOrgID}
+                            onChange={v => {
+                              const value = _.cloneDeep(props.value)
+                              _.set(value, `[${index}].TargetOrgID`, v)
+                              props.onChange(value)
+                            }}
+                          />
+                          <Select
+                            defaultValue='PERSONAL'
+                            placeholder={window.L('请选择可读范围')}
+                            value={item.ReadableRange}
+                            onChange={v => {
+                              const value = _.cloneDeep(props.value)
+                              _.set(value, `[${index}].ReadableRange`, v)
+                              props.onChange(value)
+                            }}
+                          >
+                            <Select.Option value='PERSONAL'>{window.L('个人范围可读')}</Select.Option>
+                            <Select.Option value='CURRENT'>{window.L('当前组织可读')}</Select.Option>
+                            <Select.Option value='CURRENT_FOLLOWING'>{window.L('当前及以下可读')}</Select.Option>
+                          </Select>
+                          <Select
+                            defaultValue='PERSONAL'
+                            placeholder={window.L('请选择可写范围')}
+                            value={item.WritableRange}
+                            onChange={v => {
+                              const value = _.cloneDeep(props.value)
+                              _.set(value, `[${index}].WritableRange`, v)
+                              props.onChange(value)
+                            }}
+                          >
+                            <Select.Option value='PERSONAL'>{window.L('个人范围可写')}</Select.Option>
+                            <Select.Option value='CURRENT'>{window.L('当前组织可写')}</Select.Option>
+                            <Select.Option value='CURRENT_FOLLOWING'>{window.L('当前及以下可写')}</Select.Option>
+                          </Select>
+                          <Button
+                            size={'small'}
+                            type={'danger'}
+                            icon={'minus'}
+                            shape={'circle'}
+                            className={styles.dpItemDel}
+                            onClick={() => {
+                              const value = _.cloneDeep(props.value)
+                              value.splice(index, 1)
+                              props.onChange(value)
+                            }}
+                          />
+                        </Input.Group>
+                      </div>
+                    )
+                  })}
+                  <div className={styles.dpItemAdd}>
+                    <Button
+                      size={'small'}
+                      type={'link'}
+                      icon={'plus-circle'}
+                      onClick={() => {
+                        const newVal = {
+                          key: new Date().getTime(),
+                          RoleID: _.get(props, 'rootProps.value.ID'),
+                          ReadableRange: 'CURRENT_FOLLOWING',
+                          WritableRange: 'CURRENT_FOLLOWING'
+                        }
+                        const value = _.cloneDeep(props.value) || []
+                        value.push(newVal)
+                        props.onChange(value)
+                      }}
+                    >
+                      {window.L('添加权限')}
+                    </Button>
+                  </div>
+                </div>
+              )
+            },
+            fieldOptions: {
+              rules: [
+                {
+                  required: true,
+                  message: window.L('请设置数据权限权限')
+                }
+              ]
+            }
           }
         }
       ]
     }
-  }
-
-  handleDetail (record) {
-    let value
-    if (record) {
-      value = {
-        ID: `role-edit-${record.ID}`,
-        Icon: 'team',
-        Name: window.L('编辑角色', `编辑角色（${record.Name}）`),
-        URI: `/sys/role/form/${record.ID}`,
-        Record: record
-      }
-    } else {
-      value = {
-        ID: 'role-add',
-        Icon: 'team',
-        Name: window.L('新增角色'),
-        URI: '/sys/role/form'
-      }
-    }
-    window.g_app._store.dispatch({
-      type: 'layout/addOrActivatePane',
-      payload: value
-    })
   }
 
   render () {
@@ -90,22 +207,51 @@ class Role extends React.Component {
         <FanoTable
           columns={columns}
           form={form}
-          url={'/api/role?preload=OperationPrivileges,DataPrivileges'}
-          // fillTAP={{
-          //   'add': {
-          //     onClick: () => {
-          //       this.handleDetail()
-          //     }
-          //   }
-          // }}
-          // fillRAP={{
-          //   'edit': {
-          //     show: true,
-          //     onClick: record => {
-          //       this.handleDetail(record)
-          //     }
-          //   }
-          // }}
+          url={'/api/role'}
+          drawerWidth={600}
+          listUrl={'GET /api/role?preload=OperationPrivileges,DataPrivileges'}
+          onFormRecord={record => {
+            record.ViewOperationPrivileges = _.chain(record)
+              .get('OperationPrivileges', [])
+              .map(item => item.MenuCode)
+              .value()
+          }}
+          beforeSave={(values, formRecord) => {
+            const viewOps = _.get(values, 'doc.ViewOperationPrivileges', values.ViewOperationPrivileges)
+            if (viewOps) {
+              const ops = []
+              const groupBy = _.groupBy(formRecord.OperationPrivileges, 'MenuCode')
+              for (const code of viewOps) {
+                const op = _.get(groupBy, `[${code}][0]`)
+                if (_.get(op, 'ID')) {
+                  delete groupBy[code]
+                  continue
+                }
+                ops.push({
+                  MenuCode: code,
+                  RoleID: formRecord.ID
+                })
+              }
+              if (!_.isEmpty(groupBy)) {
+                for (const code in groupBy) {
+                  const item = _.get(groupBy, `[${code}][0]`)
+                  if (!item.ID) {
+                    continue
+                  }
+                  ops.push({ ID: item.ID, DeletedAt: moment().format() })
+                }
+              }
+              if (_.has(values, 'doc')) {
+                // 修改
+                _.set(values, 'doc.OperationPrivileges', ops)
+                delete values.doc.ViewOperationPrivileges
+              } else {
+                // 新增
+                _.set(values, 'OperationPrivileges', ops)
+                delete values.ViewOperationPrivileges
+              }
+            }
+          }}
         />
       </div>
     )
